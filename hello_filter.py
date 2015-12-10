175f47956d6385600eaf5649b6e46eee8e889ce6
takao ruby <cafetakao@gmail.com>
2015-12-10 18:44:34 +0900

    versions add

--- hello_filter.py
+++ hello_filter.py
@@ -7,5 +7,11 @@ from bottle import route, run, template
 def versions(no):
     return template('versions={{no}}', no=no)
 
+
+@route('/article/<id:int>')
+def article(id):
+    return template('article={{id}}', id=id)
+
+
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True, reloader=True)
