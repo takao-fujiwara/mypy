53d76022dc90d6965710b522391779eec5514ad2
takao ruby <cafetakao@gmail.com>
2015-12-11 21:57:51 +0900

    add hello and greeting name

--- hello_filter.py
+++ hello_filter.py
@@ -13,5 +13,14 @@ def article(id):
     return template('article={{id}}', id=id)
 
 
+@route('/greeting/<name>')
+def greeting(name):
+    return template('Hello {{name}}', name=name)
+
+
+@route('/hello')
+def hello():
+    return template('Hello {{string}}', string='world')
+
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True, reloader=True)
