4aee787abc5ec0cf306b064cb3424986fc5a9248
takao ruby <cafetakao@gmail.com>
2015-12-13 11:20:58 +0900

    add photo search

--- hello_filter.py
+++ hello_filter.py
@@ -3,6 +3,16 @@
 from bottle import route, run, template
 
 
+@route('/search/<keyword:re:[a-z]+>')
+def search(keyword):
+    return template('keyword={{keyword}}', keyword=keyword)
+
+
+@route('/photo/<image_path:path>')
+def photo(image_path):
+    return template('image_path={{image_path}}', image_path=image_path)
+
+
 @route('/versions/<no:float>')
 def versions(no):
     return template('versions={{no}}', no=no)
