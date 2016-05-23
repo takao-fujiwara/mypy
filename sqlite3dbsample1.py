f7aa65ebe8065cd84895c6dbfaee06d18b197bc6
takao ruby <cafetakao@gmail.com>
2016-05-23 11:11:58 +0900

    use WHERE and add close()

--- sqlite3dbsample1.py
+++ sqlite3dbsample1.py
@@ -7,8 +7,10 @@ curs.execute(ins, ('duck', 5, 0.0))
 curs.execute(ins, ('bear', 2, 1000.0))
 curs.execute(ins, ('weasel', 1, 2000.0))
 
-curs.execute('SELECT * FROM zoo')
+curs.execute('''SELECT * FROM zoo WHERE
+    damages = (SELECT MAX(damages) FROM zoo)''')
 rows = curs.fetchall()
 print(rows)
-# curs.execute('''CREATE TABLE zoo (critter VARCHAR(20) PRIMARY KEY,
-# count INT, damages FLOAT) ''')
+
+curs.close()
+conn.close()
