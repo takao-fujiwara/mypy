4458bcf638448a7edc7823e4048d9c9f53eed776
takao ruby <cafetakao@gmail.com>
2016-07-29 08:21:09 +0900

    db sqlalchemy use program and db

---
+++ db1.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+
+engine = create_engine('sqlite:///db.sqlite3', echo=True)
+print engine
+
+metadata = MetaData()
+metadata.bind = engine
+
+menus = Table(
+    'menus', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('kcal', Integer)
+)
+
+from sqlalchemy.ext import declarative
+Base = declarative.declarative_base()
