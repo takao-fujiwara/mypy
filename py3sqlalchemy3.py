97ef4598ae14eb5033f0c4d19970755a96f8da45
takao ruby <cafetakao@gmail.com>
2016-08-20 15:45:49 +0900

    Level 3

--- py3sqlalchemy3.py
+++ py3sqlalchemy3.py
@@ -1,26 +1,30 @@
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
-conn = sa.create_engine('sqlite://')
+conn = sa.create_engine('sqlite:///zoo3.db')
+Base = declarative_base()
 
-Base = declarative_base ()
+
 class Zoo(Base):
-    __tablename__ = 'zoo'
-    critter = sa.Column('critter' , sa.String, primary_key=True)
-    count = sa.Column('count' , sa.Integer)
-    damages = sa.Column('damages' , sa.Float)
-    def __init__(self, critter, count, damages):
-        self.critter = critter
-        self.count = count
-        self.damages = damages
-    def __rep__(self):
-        return "<Zoo({}, {}, {})>".format(self.critter, self.count, self.damages)
+        __tablename__ = 'zoo'
+        critter = sa.Column('critter', sa.String, primary_key=True)
+        count = sa.Column('count', sa.Integer)
+        damages = sa.Column('damages', sa.Float)
 
+        def __init__(self, critter, count, damages):
+            self.critter = critter
+            self.count = count
+            self.damages = damages
+
+        def __rep__(self):
+            return
+            "<Zoo({}, {}, {})>".format(self.critter, self.count, self.damages)
 
 Base.metadata.create_all(conn)
 
 first = Zoo('duck', 10, 0.0)
-second =Zoo('bear', 2, 1000.0)
+second = Zoo('bear', 2, 1000.0)
 third = Zoo('weasel', 1, 2000.0)
+
 
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=conn)
