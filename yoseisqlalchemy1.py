91c037c39ad6331367e785d59f0dcbb90987512a
takao ruby <cafetakao@gmail.com>
2016-08-24 19:03:04 +0900

    yoseidokuhon From P91 To P93 Level3

--- yoseisqlalchemy1.py
+++ yoseisqlalchemy1.py
@@ -1,5 +1,34 @@
 import sqlalchemy as sa
-engine = sa.create_engine('sqlite:///post.db', echo=True)
+conn = sa.create_engine('sqlite:///post.db', echo=True)
 # print engine
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+
+class Post(Base):
+        __tablename__ = 'posts'
+        id = sa.Column(sa.Integer, primary_key=True)
+        title = sa.Column(sa.Unicode(255), nullable=False)
+        text = sa.Column(sa.UnicodeText)
+
+        def __rep__(self):
+            return
+            "<Post(title='%s', text='%s')>" % (self.title, self.text)
+
+
+Base.metadata.create_all(conn)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=conn)
+session = Session()
+
+post = Post(title=u'title_1', text=u'first')
+session.add(post)
+session.commit()
+
+session.add_all([
+    Post(title=u'title_2', text=u'second'),
+    Post(title=u'title_3', text=u'third'),
+    ])
+
+session.commit()
