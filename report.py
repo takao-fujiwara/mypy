6995cdb9630035e041d2964f1dd0bc9a0cfd9dcd
takao ruby <cafetakao@gmail.com>
2017-11-10 09:24:05 +0900

    python3 入門

---
+++ report.py
@@ -0,0 +1,5 @@
+def get_description():
+    from random import choice
+    # sleet みぞれが降る
+    possibillities = ['rain', 'snow', 'sleet', 'fog', 'sun', 'Who knows']
+    return choice(possibillities)
